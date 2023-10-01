1903ab73074708f50ba48963b9dfa1202ca22570
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2023-10-01 22:34:50 +0300

    fix js

--- js/script.js
+++ js/script.js
@@ -1,48 +1,47 @@
 let elemR;
 
+/**
+ * get R from html
+ */
 function setR() {
-    let formData = new FormData(document.getElementById("form"));
-    elemR = formData.get("r_value");
+    const formData = new FormData(document.getElementById('form'));
+    elemR = formData.get('r_value');
 }
 
-const checkInput = function () {
-    let elemY = $("#y-value-select");
+const checkInput = function() {
+    const elemY = $('#y-value-select');
 
-    let point = $("#point");
-    let value = Number(elemY.val().replace(",", "."));
+    const point = $('#point');
+    const value = Number(elemY.val().replace(',', '.'));
+    const spaceY = elemY.val().trim().length;
 
-    if (value < -5 || value > 5) {
-        document.querySelector('#error').textContent = "Y должен быть в диапазоне [-5;5]";
-        point.attr("r", 0);
-        return false;
-    } else if (!elemY.val().trim().length > 0) {
-        document.querySelector('#error').textContent = "Заполните поле";
-        point.attr("r", 0);
+    if (!spaceY > 0 || value < -5 || value > 5) {
+        document.querySelector('#error').textContent = 'Y должен быть [-5;5]';
+        point.attr('r', 0);
         return false;
     } else if (!isFinite(value)) {
-        document.querySelector('#error').textContent = "Y должен быть числом";
-        point.attr("r", 0);
+        document.querySelector('#error').textContent = 'Y должен быть числом';
+        point.attr('r', 0);
         return false;
     } else {
-        
-        document.querySelector('#error').textContent = " ";
-        let data = document.getElementById('x-value-select').value;
-        let xValue = parseInt(data);
+        document.querySelector('#error').textContent = ' ';
+        const data = document.getElementById('x-value-select').value;
+        const xValue = parseInt(data);
         setR();
-        
-        let calculatedX = 150 * (Number(xValue) / Number(elemR)) + 250;
-        let calculatedY = -(((Number(elemY.val().replace(",", ".")) * 150) / Number(elemR)) - 220);
 
-        point.attr("r", 4);
-        point.attr("cy", calculatedY);
-        point.attr("cx", calculatedX);
+        const calculatedX = 150 * (Number(xValue) / Number(elemR)) + 250;
+        const calculatedY = -(((value * 150) / Number(elemR)) - 220);
+
+        point.attr('r', 4);
+        point.attr('cy', calculatedY);
+        point.attr('cx', calculatedX);
 
 
         return true;
     }
 };
 
-const submit = function (e) {
+const submit = function(e) {
     if (!checkInput()) {
         return;
     }
@@ -53,18 +52,16 @@ const submit = function (e) {
         .then((res) => {
           document.querySelector('#answer').innerHTML = res.text;
         });
-
 };
 
-const clear = function (e) {
+const clear = function(e) {
     superagent.post('php/clear.php')
         .then((res) => {
           document.querySelector('#answer').innerHTML = res.text;
         });
-
 };
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#clearButton').addEventListener('click', clear);
     document.querySelector('#submitButton').addEventListener('click', submit);
 });
